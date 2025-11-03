585ec6315f8777ed273a26918416a60ff5a8ffb2
2400080007 <2400080007@kluniversity.in>
2025-11-03 11:37:18 +0530

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,44 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  entry: './src/index.js',
-  output: { path: path.resolve(__dirname, 'dist'), filename: 'bundle.js' },
-  devServer: { static: './public' },
-  module: { rules: [{ test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }, { test: /\.css$/, use: ['style-loader','css-loader']} ] }
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|mp3)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+  },
 };
